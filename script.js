9a62ba447f48724b6a4b2f1dbe6ead4b8394f5e3
Luís Eduardo <luiseduardorrg@gmail.com>
2022-03-15 17:49:11 -0300

    adicionando funcionalidade no js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const criarItem = (tarefa, status="") => {
+
+var banco = [
+    { 'tarefa': 'assistir jogo do vasco', 'status': 'checked' },
+    { 'tarefa': 'tomar um sorvetinho do bão', 'status': '' },
+    { 'tarefa': 'alfafa', 'status': '' },
+];
+
+const criarItem = (tarefa, status) => {
     const item = document.createElement("label");
     item.classList.add("todoItem");
     item.innerHTML = `
@@ -8,3 +15,34 @@ const criarItem = (tarefa, status="") => {
         `
     document.getElementById("todoList").appendChild(item);
 }
+
+const limparTarefa = () => {
+    const todoList = document.getElementById('todoList');
+    while (todoList.firstChild) {
+        todoList.removeChild(todoList.lastChild);
+    }
+}
+
+const atualizarTela = () => {
+    limparTarefa();
+    banco.forEach(item => criarItem(item.tarefa, item.status));
+}
+
+const addNovoItem = (evento) => {
+    const tecla = evento.key
+    const texto = evento.target.value;
+
+    function limparTexto() {
+        evento.target.value = '';
+    }
+
+    if (tecla === 'Enter') {
+        banco.push({ 'tarefa': texto, 'status': '' });
+        atualizarTela();
+        limparTexto();
+    }
+}
+
+document.getElementById('addNovoItem').addEventListener('keypress', addNovoItem);
+
+atualizarTela(); 
